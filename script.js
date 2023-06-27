f59caf3532c45d1e5a552bd50928f2d36718e9fa
fnc12345678 <137799279+fnc12345678@users.noreply.github.com>
2023-06-27 17:13:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-function toggleCategory(category) {
-  var categoryDivider = document.getElementById(category);
-  categoryDivider.classList.toggle("open");
-
-  var lessons = document.getElementsByClassName(category);
-  for (var i = 0; i < lessons.length; i++) {
-    lessons[i].classList.toggle("open");
-  }
+function copyContent() {
+      // Seleziona l'elemento con la classe "content"
+      var content = document.querySelector('.content');
+      
+      // Crea un elemento textarea
+      var textarea = document.createElement('textarea');
+      
+      // Imposta il valore del textarea come il testo della lezione
+      textarea.value = content.textContent;
+            
+      // Aggiungi il textarea al documento
+      document.body.appendChild(textarea);
+      
+      // Seleziona il testo nel textarea
+      textarea.select();
+      
+      // Copia il testo negli appunti del dispositivo
+      document.execCommand('copy');
+      
+      // Rimuovi il textarea dal documento
+      document.body.removeChild(textarea);
 }
-
-function toggleAnswer(questionNumber) {
-  const answer = document.getElementById(`answer${questionNumber}`);
-  answer.style.display = answer.style.display === "block" ? "none" : "block";
-}
-
